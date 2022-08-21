8aa108d376e561a7d7ccba0f556829f83a0a542c
flav0000 <104461297+flav0000@users.noreply.github.com>
2022-08-21 20:05:52 +0200

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -2,13 +2,13 @@ const config = require(`../../.ghost.json`).production;
 module.exports = {
     siteUrl:
         process.env.NODE_ENV === `production`
-            ? process.env.SITE_URL || config.siteUrl || `http://localhost:8000`
-            : `http://localhost:8000`, // Site domain. Do not include a trailing slash!
+            ? process.env.SITE_URL || config.siteUrl || `http://imiel.work`
+            : `http://imiel.work`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `imiel.work`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `imiel.work`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
