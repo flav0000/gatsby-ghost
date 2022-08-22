12e0bd9e28390523f369a101ff11f4347d2529e3
flav0000 <104461297+flav0000@users.noreply.github.com>
2022-08-22 08:54:22 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ try {
 
 if (
     process.env.NODE_ENV === `production` &&
-    config.siteUrl === `http://localhost:8080` &&
+    config.siteUrl === `http://localhost:8000` &&
     !process.env.SITEURL
 ) {
     throw new Error(
